f36ed1647d03b3163644d64d51106e4b42072da1
Jason Yoo <jason@radishfiction.com>
2022-06-16 16:42:13 +0900

    Change cURL

--- scripts/watch.sh
+++ scripts/watch.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-exit $(curl -s https://api.github.com/repos/naver/fe-news/commits/REL1_36 | jq -r "((now - (.commit.author.date | fromdateiso8601) )  / (60*60*24)  | trunc)")
+exit $(curl -s https://api.github.com/repos/naver/fe-news/commits/master | jq -r "((now - (.commit.author.date | fromdateiso8601))  / (60*60*24)  | trunc)")
